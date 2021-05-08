8791f730b307c499a1723b3da1d568be127aedca
Ayush Thapa <eplramailo@gmail.com>
2021-05-08 06:40:21 +0545

    added product futsal shoes

--- pages/products.jsx
+++ pages/products.jsx
@@ -4,6 +4,8 @@ export default function Products() {
     return (
         <div>
             <h2>Products from epl ramailo</h2>
+            <h3>Products</h3>
+            <p>Footsal shoes</p>
         </div>
     )
 }
